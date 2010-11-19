13838f352211419378f8e50f7160566a9fd1a141
unknown <Bryon@.(none)>
2010-11-19 10:50:51 -0800

    Automated GeneralTab UI, activated togglebits

--- src/net/jonrichards/batteryapp/ui/GeneralActivity.java
+++ src/net/jonrichards/batteryapp/ui/GeneralActivity.java
@@ -3,12 +3,15 @@ package net.jonrichards.batteryapp.ui;
 import net.jonrichards.batteryapp.system.DS2784Battery;
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class GeneralActivity extends Activity {
+	
+	private final Handler mHandler = new Handler();
 	
 	//Declaring all text views needed for general tab
 	private TextView voltage;
@@ -19,11 +22,12 @@ public class GeneralActivity extends Activity {
 	private TextView full40;
 	private TextView chargecurrent;
 	private TextView emptyvolt;
-	private Button parametersbutton;
+	private TextView statusreg;
 
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.generallayout);
+        
         
         //Defining all text views and linking to UI elements
         voltage = (TextView)findViewById(R.id.txtVoltage);
@@ -33,54 +37,57 @@ public class GeneralActivity extends Activity {
         age = (TextView)findViewById(R.id.txtAge);
         full40 = (TextView)findViewById(R.id.txtFull40);
         chargecurrent = (TextView)findViewById(R.id.txtMinChgCurrent);
-        emptyvolt = (TextView)findViewById(R.id.txtAEvolt);
-        
-        //this button is being added to the UI only temporarily to test populating the views
-        parametersbutton = (Button)findViewById(R.id.btnParameters);
-        parametersbutton.setOnClickListener(new OnClickListener() {
-        	public void onClick(View v) {
-        		//These OnClick events will be moved to the their own function in final version
-        		DS2784Battery battery_info = new DS2784Battery();
-        		
-        		//Populate voltage
-        		String voltagetext = battery_info.getVoltage();
-        		voltage.setText(voltagetext);
-        		
-        		//Populate current
-        		String currenttext = battery_info.getCurrent();
-        		current.setText(currenttext);
-        		
-        		//Populate full40
-        		String full40text = battery_info.getFull40();
-        		full40.setText(full40text);
-        		
-        		//Populate temperature
-        		//String temperaturetext = battery_info.getTemperature();
-        		//temperature.setText(temperaturetext);
-        		
-        		//Populate age
-        		String agetext = battery_info.getDumpRegister(20);
-        		int ageconverted = (Integer.parseInt(agetext,16))*100/128;
-        		age.setText(Integer.toString(ageconverted));
-        		
-        		//Populate percent
-        		String percenttext = battery_info.getDumpRegister(6);
-        		percent.setText(Integer.toString(Integer.parseInt(percenttext,16)));
-        		
-        		//Populate min charge current
-        		String chargecurrenttext = battery_info.getDumpRegister(53);
-        		int chargecurrentconverted = (Integer.parseInt(chargecurrenttext,16))*50/15;
-        		chargecurrent.setText(Integer.toString(chargecurrentconverted));
-        		
-        		//Populate AEvolt
-        		String emptyvolttext = battery_info.getDumpRegister(54);
-        		int emptyconverted = (Integer.parseInt(emptyvolttext,16))*1952/100;
-        		emptyvolt.setText(Integer.toString(emptyconverted));
-        	}
-        });
+        emptyvolt = (TextView)findViewById(R.id.txtAEvolt);       
+        statusreg = (TextView)findViewById(R.id.txtStatusReg);        
 
-        //TextView textview = new TextView(this);
-        //textview.setText("This is the Events tab");
-        //setContentView(textview);
+        mHandler.postDelayed(mUpdateUITimerTask, 60 * 1000);
+        getUIText();
+
     }
+	//Our runnable to update the UI, polled every 10 seconds
+	private final Runnable mUpdateUITimerTask = new Runnable() {
+	    public void run() {
+	    	getUIText();
+	        mHandler.postDelayed(mUpdateUITimerTask, 60 * 1000);
+	    }
+	};
+	//Function for retrieving the UI text
+	protected void getUIText() {
+		DS2784Battery battery_info = new DS2784Battery();
+		
+		//Populate voltage
+		String voltagetext = battery_info.getVoltage();
+		voltage.setText(voltagetext);
+		
+		//Populate current
+		String currenttext = battery_info.getCurrent();
+		current.setText(currenttext);
+		
+		//Populate full40
+		String full40text = battery_info.getFull40();
+		full40.setText(full40text);
+		
+		//Populate temperature
+		//String temperaturetext = battery_info.getTemperature();
+		//temperature.setText(temperaturetext);
+		
+		//Populate age
+		String agetext = battery_info.getDumpRegister(20);
+		int ageconverted = (Integer.parseInt(agetext,16))*100/128;
+		age.setText(Integer.toString(ageconverted));
+		
+		//Populate percent
+		String percenttext = battery_info.getDumpRegister(6);
+		percent.setText(Integer.toString(Integer.parseInt(percenttext,16)));
+		
+		//Populate min charge current
+		String chargecurrenttext = battery_info.getDumpRegister(53);
+		int chargecurrentconverted = (Integer.parseInt(chargecurrenttext,16))*50/15;
+		chargecurrent.setText(Integer.toString(chargecurrentconverted));
+		
+		//Populate AEvolt
+		String emptyvolttext = battery_info.getDumpRegister(54);
+		int emptyconverted = (Integer.parseInt(emptyvolttext,16))*1952/100;
+		emptyvolt.setText(Integer.toString(emptyconverted));
+	}
 }
--- src/net/jonrichards/batteryapp/ui/LearnModeActivity.java
+++ src/net/jonrichards/batteryapp/ui/LearnModeActivity.java
@@ -1,17 +1,120 @@
 package net.jonrichards.batteryapp.ui;
 
+import net.jonrichards.batteryapp.system.DS2784Battery;
 import android.app.Activity;
 import android.os.Bundle;
-import net.jonrichards.batteryapp.ui.R;
+import android.os.Handler;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class LearnModeActivity extends Activity {
+	
+	private ToggleButton CHGTFlight;
+	private ToggleButton AEFlight;
+	private ToggleButton SEFlight;
+	private ToggleButton LEARNFlight;
+	private ToggleButton UVFlight;
+	private ToggleButton PORFlight;
+	
+	private RadioButton learnon;
+	private RadioButton learnoff;
+	
+	private boolean learnmode = false;
+	private int SAMPLE_POLL;
+
+	private final Handler mHandler = new Handler();
+
+	private TextView livevoltage;
+	
 
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.learnmodelayout);
+        
+        livevoltage = (TextView)findViewById(R.id.txtRealTimeVoltage);
+        
+        CHGTFlight = (ToggleButton) findViewById(R.id.CHGTFlight);
+        AEFlight = (ToggleButton) findViewById(R.id.AEFlight);
+        SEFlight = (ToggleButton) findViewById(R.id.SEFlight);
+        LEARNFlight = (ToggleButton) findViewById(R.id.LEARNFlight);
+        UVFlight = (ToggleButton) findViewById(R.id.UVFlight);
+        PORFlight = (ToggleButton) findViewById(R.id.PORFlight);
+        
+        learnon=(RadioButton)findViewById(R.id.btnLearnOn);
+		learnoff=(RadioButton)findViewById(R.id.btnLearnOff);        
+        ;
+        
 
-        //TextView textview = new TextView(this);
-        //textview.setText("This is the Events tab");
-        //setContentView(textview);
+        mHandler.postDelayed(mUpdateUITimerTask, 3 * 500);
+        getUIText();        
     }
+	
+	
+
+	
+	//Our runnable to update the UI, polled every 10 seconds
+	private final Runnable mUpdateUITimerTask = new Runnable() {
+	    public void run() {
+	    	getUIText();
+	        mHandler.postDelayed(mUpdateUITimerTask, 3 * 500);
+	    }
+	};
+	
+	//Function for retrieving the UI text
+	protected void getUIText() {
+		DS2784Battery battery_info = new DS2784Battery();
+		
+		//Populate voltage
+		String voltagetext = battery_info.getVoltage();
+		livevoltage.setText(voltagetext);
+		
+		//Populate status register flags
+		int CHGTF = battery_info.getStatusRegister(7);
+		if (CHGTF > 0) {
+			CHGTFlight.setChecked(true);		
+		}
+		else {
+			CHGTFlight.setChecked(false);
+		}
+		
+		int AEF = battery_info.getStatusRegister(6);
+		if (AEF > 0) {
+			AEFlight.setChecked(true);		
+		}
+		else {
+			AEFlight.setChecked(false);
+		}
+		int SEF = battery_info.getStatusRegister(5);
+		if (SEF > 0) {
+			SEFlight.setChecked(true);		
+		}
+		else {
+			SEFlight.setChecked(false);
+		}		
+		int LEARNF = battery_info.getStatusRegister(4);
+		if (LEARNF > 0) {
+			LEARNFlight.setChecked(true);		
+		}
+		else {
+			LEARNFlight.setChecked(false);
+		}		
+		int UVF = battery_info.getStatusRegister(2);
+		if (UVF > 0) {
+			UVFlight.setChecked(true);		
+		}
+		else {
+			UVFlight.setChecked(false);
+		}		
+		int PORF = battery_info.getStatusRegister(1);
+		if (PORF > 0) {
+			PORFlight.setChecked(true);		
+		}
+		else {
+			PORFlight.setChecked(false);
+		}		
+		
+	}	
+		
 }
